348aa332bf4d3b31184348ee553c9a982043e8e1
Arley Rivas G <arrivasg@unipacifico.edu.co>
2021-12-28 21:41:46 -0500

    tipado de interface, con interface en su interior

---
+++ src/ejercicios/04-tarea-tipados.ts
@@ -0,0 +1,34 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+
+
+interface SuperHeroe {
+    nombre: string,
+    edad: number,
+    direccion:DireccionHeroe,
+    mostrarDireccion: () => string
+}
+
+interface DireccionHeroe {
+    calle: string,
+    pais: string,
+    ciudad: string
+}
+
+const superHeroe:SuperHeroe = {
+    nombre: 'Spiderman',
+    edad: 30,
+    direccion: {
+        calle: 'Main St',
+        pais: 'USA',
+        ciudad: 'New York'
+    },
+    mostrarDireccion(): string{
+        return this.nombre + ', '+ this.direccion.ciudad + ', ' + this.direccion.pais;
+    }
+}
+
+const direccion = superHeroe.mostrarDireccion();
+console.log(direccion);
